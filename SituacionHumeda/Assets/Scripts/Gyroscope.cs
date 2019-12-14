163d7b3e860c2ce986a8b02e8a522ac33e390380
Carlos <carlete96@hotmail.com>
2019-12-14 12:45:56 +0100

    Añadido giroscopio

---
+++ SituacionHumeda/Assets/Scripts/Gyroscope.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gyroscope : MonoBehaviour
+{
+
+    public bool gyroAvailable = false;
+
+    private UnityEngine.Gyroscope gyro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gyro = Input.gyro;
+        gyroAvailable = SystemInfo.supportsGyroscope;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics2D.gravity = gyro.gravity;
+        Debug.Log(gyro.rotationRate);
+    }
+}

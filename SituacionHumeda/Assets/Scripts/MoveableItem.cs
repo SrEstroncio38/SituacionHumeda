1d15d84665263eef4033f6bc93151f4185113a1c
David Fontela <d.fontela.1998@gmail.com>
2019-12-16 01:05:38 +0100

    Movible y Titulos

---
+++ SituacionHumeda/Assets/Scripts/MoveableItem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableItem : MonoBehaviour
+{
+    private Vector3 startPos;
+    private Vector3 mPos;
+    private bool held = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = this.transform.localPosition;
+        mPos = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (held == true)
+        { 
+            mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mPos.z = 0;
+            this.gameObject.transform.localPosition = mPos - startPos;
+        }
+    }
+
+    public void OnMouseDown()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - this.transform.localPosition;
+            startPos.z = 0;
+            held = true;
+        }
+        
+    }
+
+    public void OnMouseUp()
+    {
+        held = false;
+    }
+}
